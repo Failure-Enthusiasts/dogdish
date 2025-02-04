c219ff83b14f6355b11d5fdcf92be2f074c7e1f3
bradleyjay <bradleyjshields@gmail.com>
2025-02-03 19:02:17 -0500

    Added basic ORM

--- database_handler/database_handler.py
+++ database_handler/database_handler.py
@@ -1,5 +1,9 @@
 # boilerplate flask app: https://flask.palletsprojects.com/en/stable/quickstart/
-from flask import Flask
+from flask import Flask, request
+from database_handler.db import Event
+from pydantic import ValidationError
+
+
 
 app = Flask(__name__)
 
@@ -9,9 +13,26 @@ def hello_world():
 
 
 # route: takes in event data
-@app.route("/intake")
+@app.route("/intake", methods=["POST"])
 def intake():
-    return "bleep bloop I now have data"
+    incoming_event = request.json  # receives JSON
+    print(f"Incoming event: {incoming_event}")
+
+
+    # verify it's an Event
+    try:
+        is_event = Event.model_validate_json(incoming_event['event'])
+        if is_event:
+            # post to the DB as incoming_event['event']
+            print(f"is_event: {is_event}")
+            return "Event Created!"
+
+    except ValidationError as e:
+        # raise an error
+        return f"Validation Error: {e}"
+    
+    return "Event not created!"
+    
 
 
 # route: responds to UI calls with data
